5f60b2c0dda3ba579d3ea09deacde6e1c46719fe
Niko_Zhang <593177876@qq.com>
2018-08-07 00:42:44 +0800

    手写了一个Require

--- NODE/2.module/require.js
+++ NODE/2.module/require.js
@@ -18,13 +18,57 @@ function Module(id) {
 }
 
 // exports = module.exports
-Module.wrapper = [];
-Module.wrap = function (script) {};
-Module._extensions = {};
-Module._resolveFilename = function (relativePath) {};
+Module.wrapper = [
+    '(function(exports,module,require){}',
+    '\n})'
+];
+Module.wrap = function (script) {
+    const [start, end] = this.wrapper;
+    return `${start+script+end}`;
+};
+Module._extensions = {
+    '.js' (module) {
+        const str = fs.readFileSync(module.id, 'utf-8'); // 读取文件内容
+        const fnStr = this.wrap(str); // 返回的是函数字符串
+        const hanlder = vm.runInThisContext(fnStr); // 让产生的函数执行
+        hanlder.call(module.exports, module.exports, module, req); // 会在内部把结果赋值到exports属性上
+    },
+    '.json' (module) {
+        const str = fs.readFileSync(module.id, 'utf-8'); // 读取文件内容
+        const json = JSON.parse(str);
+        module.exports = json;
+    },
+    '.txt' (module) {
+        console.log(module);
+        return
+    }
+};
+Module._resolveFilename = function (relativePath) {
+    const p = path.resolve(__dirname, relativePath); // 把转入的相对路径转为绝对路径
+    const exists = fs.existsSync(p); // 如果有路径，则直接返回路径
+    if (exists) {
+        return p;
+    }
+
+    const keys = Object.keys(this._extensions); // 生成后缀名数组
+    let r = false;
+    for (const key of keys) { // 循环后缀，拼接好文件路径
+        const realPath = path + key;
+        let exists = fs.existsSync(realPath);
+        if (exists) {
+            r = realPath;
+            break;
+        }
+    }
+    if (!r) throw new Error('文件不存在');
+    return r;
+};
 Module._cache = {};
 Module.prototype.load = function (filename) {
+    console.log(path.extname(filename));
 
+    let extension = path.extname(filename);
+    Module._extensions[extension](this);
 }
 
 function req(p) {
@@ -45,3 +89,6 @@ function req(p) {
         console.log(err);
     }
 }
+
+let test = req('./test.txt');
+console.log(test);
