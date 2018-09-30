86fc79f8b7a87e29585b4eabb7a6ec07717f1780
NikoZhang <593177876@qq.com>
2018-09-30 16:19:02 +0800

    proxy

---
+++ ES6/proxy.js
@@ -0,0 +1,38 @@
+/* oberseve */
+function oberseve(object) {
+    if (typeof object !== 'object') {
+        return;
+    }
+
+    for (const key in object) {
+        defineReactive(object, key, object[key]);
+        oberseve(object[key]);
+    }
+}
+
+/* defineReactive */
+function defineReactive(target, property, value) {
+    Object.defineProperty(target, property, {
+        get() {
+            return value;
+        },
+        set(newValue) {
+            value = newValue;
+            console.log(`数据更新了:${value}`)
+        }
+    })
+}
+
+oberseve(obj);
+
+obj.name = 'BIO';
+obj.name = '卡洛斯';
+obj.name = 'hello';
+obj.name = 'word';
+
+/* 
+  
+*/
+
+
+/* proxy */
