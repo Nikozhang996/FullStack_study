39cad30c2ccc42c38c032a6fcc8280a7d128a380
Niko_Zhang <593177876@qq.com>
2018-10-24 01:08:04 +0800

    树状图

---
+++ ES6/test.js
@@ -0,0 +1,60 @@
+const arr = [{
+        id: 1,
+        value: '111',
+        child: [{
+                id: 11,
+                value: '222',
+            },
+            {
+                id: 22,
+                value: '333',
+            },
+            {
+                id: 33,
+                value: '444',
+            },
+        ]
+    },
+    {
+        id: 2,
+        value: '222',
+        child: [{
+                id: 22,
+                value: '2222',
+            },
+            {
+                id: 23,
+                value: '2323',
+            },
+            {
+                id: 24,
+                value: '2424',
+            },
+        ]
+    },
+    {
+        id: 3,
+        value: '333',
+        child: [{
+                id: 33,
+                value: '333',
+            },
+            {
+                id: 34,
+                value: '3434',
+            },
+            {
+                id: 35,
+                value: '3535',
+            },
+            {
+                id: 36,
+                value: '3636',
+                child: [{
+                    id: 666,
+                    value: '666',
+                }]
+            }
+        ]
+    },
+]
