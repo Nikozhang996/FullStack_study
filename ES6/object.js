484a2d9162394c9d333b1ff1c9d31514ad53e587
Niko_Zhang <593177876@qq.com>
2018-11-08 12:18:01 +0800

    object

---
+++ ES6/object.js
@@ -0,0 +1,16 @@
+const params = {
+    id: 1,
+    name: 'zjk',
+    age: 20,
+    hobby: ['photo', 'App', 'music'],
+    test: '',
+    test1: null,
+}
+
+function vaildate(target) {
+    Object.values(target).forEach(item => {
+        console.log(item == '');
+    });
+}
+
+vaildate(params)
