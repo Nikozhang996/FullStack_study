84c5b844b212d2387ead54e3eed46c3365ef633d
Niko_Zhang <593177876@qq.com>
2018-11-20 00:21:28 +0800

    single

---
+++ DesignPattern/4,.单例模式/book.js
@@ -0,0 +1,32 @@
+/* 简单单例 */
+class Singleton {
+    constructor(name) {
+        this.name = name;
+    }
+    static getInstance(name) {
+        if (!this.instance) {
+            this.instance = new Singleton(name)
+        }
+        return this.instance;
+    }
+    getName() {
+        console.log(this.name);
+    }
+}
+
+class Singleton2 {
+    constructor(name) {
+        this.name = name;
+    }
+   
+    // static
+    
+    getName() {
+        console.log(this.name);
+    }
+}
+
+
+// let s1 = Singleton.getInstance('s1');
+// let s2 = Singleton.getInstance('s2');
+// console.log(s1 === s2);
