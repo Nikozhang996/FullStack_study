c8bcc6ecc4c4a4ee4433f4d06d0474a7a43eca81
Niko_Zhang <593177876@qq.com>
2018-11-26 23:22:37 +0800

    1.callback.js

--- DesignPattern/4,.单例模式/book.js
+++ DesignPattern/4,.单例模式/book.js
@@ -1,29 +1,29 @@
 /* 简单单例 */
 class Singleton {
-    constructor(name) {
-        this.name = name;
+  constructor(name) {
+    this.name = name;
+  }
+  static getInstance(name) {
+    if (!this.instance) {
+      this.instance = new Singleton(name)
     }
-    static getInstance(name) {
-        if (!this.instance) {
-            this.instance = new Singleton(name)
-        }
-        return this.instance;
-    }
-    getName() {
-        console.log(this.name);
-    }
+    return this.instance;
+  }
+  getName() {
+    console.log(this.name);
+  }
 }
 
 class Singleton2 {
-    constructor(name) {
-        this.name = name;
-    }
-   
-    // static
-    
-    getName() {
-        console.log(this.name);
-    }
+  constructor(name) {
+    this.name = name;
+  }
+
+  // static
+
+  getName() {
+    console.log(this.name);
+  }
 }
 
 
---
+++ Promise/2.callback.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+
+/* fs.readFile(path.join(__dirname + '/data/name.txt'), 'utf-8', function (err, data) {
+    console.log(data);
+}); */
