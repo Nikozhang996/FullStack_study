2802f6e04ed049451eacbbae367508416c378a64
Niko_Zhang <593177876@qq.com>
2019-01-08 23:22:37 +0800

    手写完promise

---
+++ Promise/2.promise/3.case.js
@@ -0,0 +1,24 @@
+const Promise = require('./3.promise');
+
+let p = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve(10);
+  }, 1000);
+});
+
+p
+  .then(
+    function (value) {
+      console.log('suc:' + value);
+      return value ** 2;
+    },
+    function (reason) {
+      console.log('err:' + reason);
+    })
+  // .then(
+  //   function (value) {
+  //     console.log('suc:' + value);
+  //   },
+  //   function (reason) {
+  //     console.log('err:' + reason);
+  //   });
