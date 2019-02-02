2d22e94e0a0c8bc057bee76c3c0f7110ebe11959
NikoZhang <593177876@qq.com>
2019-02-02 17:44:17 +0800

    genrator

---
+++ Promise/4.generator/1.generator.js
@@ -0,0 +1,57 @@
+const likeArray = {
+    0: 1,
+    1: 2,
+    2: 3,
+    3: 4,
+    length: 4,
+    [Symbol.iterator]() {
+        console.log(this);
+        let flag = false;
+        let index = 0;
+        let that = this;
+        return {
+            next() {
+                return {
+                    done: index === that.length,
+                    value: that[index++]
+                }
+            }
+        }
+    }
+}
+
+/**
+ * * 表示这个函数是个生成器函数，一般配合yield使用
+ */
+const likeArray2 = {
+    0: 1,
+    1: 2,
+    2: 3,
+    3: 4,
+    length: 4,
+    [Symbol.iterator]: function* () {
+        while (index !== this.length) {
+            yield this[index++];
+        }
+    }
+}
+
+function* gen(value) {
+    yield value++;
+    yield value++;
+    yield value++;
+    yield value++;
+}
+
+const it = gen(1);
+
+let flag = false;
+
+do {
+    let {
+        value,
+        done
+    } = it.next();
+    flag = done;
+    console.log(value);
+}while(!flag)
---
+++ Promise/4.generator/2.generator.js
@@ -0,0 +1,14 @@
+function* gen(value) {
+    let a = yield value++;
+    console.log(a);
+    let b = yield value + a;
+    console.log(b);
+    let c = yield value + b;
+    console.log(c);
+}
+
+const it = gen(1);
+
+it.next(7);
+it.next(8);
+it.next(9);
