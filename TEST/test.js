5b5833c4f7b45d08b7ae89257871fb4c92d1a1da
593177876@qq.com <afzllqll206>
2019-04-30 18:22:24 +0800

    'ig'

--- TEST/test.js
+++ TEST/test.js
@@ -1,9 +1,5 @@
 const url = 'https://www.vipthink.cn/index/ad/ad_index/adId/10821?callback=http://track.sigmob.cn/track?c%3DCiRmOWVkMmI4ZC02MGYzLTExZTktOWY0YS0wMDE2M2UwYzIzZDQiATEqC2UwMDAxYzc1ZjRhOiMKAjUyEgQ0NzMwGgUxMzM2MyADKIgnMIgnUgM3NzhaAzI4MkgBUAFafwokODUwQ0M0N0UtMkM2Qy00RTBELTlDRUItNEM1QjYzMTQ3MjkxEiQ3QzhBN0IwRi1BMEMxLTQ2OUQtOUMwMy00MDlBN0ZGMUREN0MaBDEyMzJKJDg1MENDNDdFLTJDNkMtNEUwRC05Q0VCLTRDNUI2MzE0NzI5MVIFQXBwbGViBDEyNDFqDDYxLjQ5LjUxLjE1Ng%26e%3Dactive%26p%3DOML9xjIE2GpXa5Auktzzng#'
 
-let start = url.indexOf('?') + 1;
+const str = new URL(url);
 
-let result = url.slice(start, -1);
-
-let obj  = new URL(url);
-
-console.log(obj.searchParams);
+console.log(str);
