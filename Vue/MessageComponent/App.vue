fea9f34c0a87221da6fc306079c4f54e4a037123
zhangjianke <jianke.zhang@happy-seed.com>
2019-06-18 10:37:02 +0800

    study message-components

---
+++ Vue/MessageComponent/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="onShowMessage">点击调用</button>
+  </div>
+</template>
+
+<script>
+  // import Message from './components/Message';
+  import Test from './components/Test';
+
+  export default {
+    name: "App",
+    components: {
+      Test,
+      // Message,
+    },
+    methods: {
+      onShowMessage() {
+        console.log(this);
+        this.$message.info({
+          message: 'i am info',
+          duration: 3000
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  body,html {
+    background-color: #666666;
+    color: #00A7AA;
+  }
+</style>
