b6d8b4d5f30bcd7cc40de14ef1cc6689a652cb8e
NikoZhang <593177876@qq.com>
2019-07-15 11:22:53 +0800

    change

--- LeeCode/1. 两数之和.js
+++ LeeCode/1. 两数之和.js
@@ -1,19 +1,19 @@
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- 
-    示例:
-    给定 nums = [2, 7, 11, 15], target = 9
-    因为 nums[0] + nums[1] = 2 + 7 = 9
-    所以返回 [0, 1]
-    https://leetcode-cn.com/problems/two-sum/solution/liang-shu-zhi-he-by-leetcode-2/
+
+ 示例:
+ 给定 nums = [2, 7, 11, 15], target = 9
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
+ https://leetcode-cn.com/problems/two-sum/solution/liang-shu-zhi-he-by-leetcode-2/
  */
 
 /*************************************************************************************************/
 
 const nums = [2, 7, 11, 15];
 
-const twoSum = function(nums, target) {
+const twoSum = function (nums, target) {
   if (!Array.isArray(nums)) {
     console.log("nums非数组");
     return;
@@ -35,17 +35,18 @@ const twoSum = function(nums, target) {
   console.log("结果不存在");
 };
 
-const twoSum2 = function(nums, target) {
+const twoSum2 = function (nums, target) {
   // 将数组转为哈希表，以值为key，索引为value
   let hashMap = new Map();
   nums.forEach((item, index) => hashMap.set(item, index));
 
+  // 遍历数组，并在哈希表中查找是否存在剩余项
   for (let i = 0; i < nums.length; i++) {
-    let complementValue = target - nums[i], // 剩余值
+    let complementValue = target - nums[i], // 剩余值（目标值）
       complementFlag = hashMap.has(complementValue), // 是否存在剩余值
       complementKey = hashMap.get(complementValue); // 剩余值索引
 
-    if (complementFlag && complementKey != i) {
+    if (complementFlag && complementKey !== i) {
       return [i, complementKey];
     }
   }
@@ -53,6 +54,26 @@ const twoSum2 = function(nums, target) {
   return "没有数据";
 };
 
-console.log(twoSum2(nums, 9));
-console.log(twoSum2(nums, 18));
-console.log(twoSum2(nums, 17));
+const twoSum3 = function (nums, target, i = 0, maps = {}) {
+  const map = maps;
+
+  //
+  if (map[target - nums[i]] >= 0) {
+    return [map[target - nums[i]], i]
+  } else {
+    map[nums[i]] = i;
+    i++;
+
+    if (i < nums.length - 1) {
+      return twoSum3(nums, target, i, map)
+    } else {
+      throw 'error: twoSum is not find'
+    }
+  }
+};
+
+
+console.log(twoSum3(nums, 9));
+// console.log(twoSum3(nums, 18));
+// console.log(twoSum3(nums, 17));
+
