02d5c55b28f947a28aceb41dc1f024b9c794cf0c
NikoZhang <593177876@qq.com>
2019-11-21 11:22:50 +0800

    mkdirSync

--- NODE/fs/mkdir.js
+++ NODE/fs/mkdir.js
@@ -1,2 +1,21 @@
 const fs = require("fs");
+const fsPromise = require("fs").promises;
 const path = require("path");
+
+const pathUrl = "c/d/e/f/g";
+
+function mkdirSync(pathUrl) {
+  const pathArr = pathUrl.split("/");
+  for (let i = 0; i < pathArr.length; i++) {
+    const currentPath = pathArr.slice(0, i + 1).join("/"),
+      absPath = path.resolve(__dirname, currentPath);
+
+    try {
+      fs.accessSync(absPath);
+    } catch (error) {
+      fs.mkdirSync(absPath);
+    }
+  }
+}
+
+mkdirSync(pathUrl);
