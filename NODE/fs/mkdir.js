3aa996a1a450dee0797306dbced81be3e8b8e81f
NikoZhang <593177876@qq.com>
2019-12-20 18:42:04 +0800

    数组去重

--- NODE/fs/mkdir.js
+++ NODE/fs/mkdir.js
@@ -3,7 +3,7 @@ const fsPromise = require("fs").promises;
 const path = require("path");
 
 const pathUrl = "c/d/e/f/g";
-
+// 同步创建
 function mkdirSync(pathUrl) {
   const pathArr = pathUrl.split("/");
   for (let i = 0; i < pathArr.length; i++) {
@@ -17,22 +17,24 @@ function mkdirSync(pathUrl) {
     }
   }
 }
-
+// 异步创建
 function mkdir(pathUrl, callback) {
+  // 分割路径
   const pathArr = pathUrl.split("/");
   console.log(path.resolve(__dirname, pathArr[0]));
-  
+  // 首先判断该目录是否存在，不存在则创建
   fs.access(path.resolve(__dirname, pathArr[0]), function(err, data) {
     if (err) {
       console.log("文件夹已存在");
     } else {
+      // 递归创建指定目录文件夹
       function next(index) {
         // 递归先确定终止条件
         if (pathArr.length === index) return callback();
         const currentPath = pathArr.slice(0, ++index).join("/");
         const absPath = path.resolve(__dirname, currentPath);
 
-        fs.access(currentPath, function(err, data) {
+        fs.access(currentPath, function(err, data) {1
           if (err) {
             fs.mkdir(currentPath, function() {
               next(index);
