d53b9ffba9b0b164c3bcd70b557cb80b433f9735
NikoZhang <593177876@qq.com>
2019-12-24 17:38:44 +0800

    update

--- NODE/fs/mkdir.js
+++ NODE/fs/mkdir.js
@@ -21,25 +21,28 @@ function mkdirSync(pathUrl) {
 function mkdir(pathUrl, callback) {
   // 分割路径
   const pathArr = pathUrl.split("/");
-  console.log(path.resolve(__dirname, pathArr[0]));
   // 首先判断该目录是否存在，不存在则创建
   fs.access(path.resolve(__dirname, pathArr[0]), function(err, data) {
     if (err) {
+      console.log(path.resolve(__dirname, pathArr[0]));
       console.log("文件夹已存在");
     } else {
       // 递归创建指定目录文件夹
       function next(index) {
         // 递归先确定终止条件
-        if (pathArr.length === index) return callback();
+        if (index === pathArr.length) return callback();
+        // 获取当前路径
         const currentPath = pathArr.slice(0, ++index).join("/");
+
         const absPath = path.resolve(__dirname, currentPath);
 
-        fs.access(currentPath, function(err, data) {1
+        fs.access(currentPath, (err, data) => {
           if (err) {
             fs.mkdir(currentPath, function() {
-              next(index);
+              next(index); // 当前创建完毕后 创建下一次即可
             });
           } else {
+            // 如果存在，创建下一层
             next(index);
           }
         });
@@ -50,6 +53,26 @@ function mkdir(pathUrl, callback) {
   });
 }
 
-mkdir(pathUrl, function() {
+const mkdirSource = (pathUrl, cb) => {
+  // next
+  let pathArr = pathUrl.split("/");
+  function next(index) {
+    // 递归必须要有终止条件
+    if (index === pathArr.length) return cb();
+    let current = pathArr.slice(0, ++index).join("/");
+    fs.access(current, (err, data) => {
+      if (err) {
+        fs.mkdir(current, function() {
+          next(index); // 当前创建完毕后 创建下一次即可
+        });
+      } else {
+        // 如果存在，创建下一层
+        next(index);
+      }
+    });
+  }
+  next(0);
+};
+mkdirSource(pathUrl, function() {
   console.log("mkdir success");
 });
