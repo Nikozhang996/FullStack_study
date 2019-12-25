d69a979e47044f1868f354a861b217640ee0d00b
NikoZhang <593177876@qq.com>
2019-12-25 13:58:47 +0800

    update

--- NODE/fs/mkdir.js
+++ NODE/fs/mkdir.js
@@ -3,6 +3,84 @@ const fsPromise = require("fs").promises;
 const path = require("path");
 
 const pathUrl = "c/d/e/f/g";
+const pathMap = [
+  {
+    id: "c",
+    name: "C",
+    children: [
+      {
+        id: "c1",
+        name: "C1"
+      },
+      {
+        id: "c2",
+        name: "C2"
+      },
+      {
+        id: "c3",
+        name: "C3",
+        children: [
+          {
+            id: "e1",
+            name: "E1"
+          },
+          {
+            id: "e2",
+            name: "E2"
+          },
+          {
+            id: "e3",
+            name: "E3"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: "d",
+    name: "D",
+    children: [
+      {
+        id: "d1",
+        name: "D1"
+      },
+      {
+        id: "d2",
+        name: "D2"
+      },
+      {
+        id: "d3",
+        name: "D3"
+      }
+    ]
+  },
+  {
+    id: "e",
+    name: "E",
+    children: [
+      {
+        id: "e1",
+        name: "E1"
+      },
+      {
+        id: "e2",
+        name: "E2"
+      },
+      {
+        id: "e3",
+        name: "E3"
+      }
+    ]
+  }
+];
+
+// 解析树级路径
+function parsePath(data) {
+  console.log(data);
+}
+
+parsePath(pathMap);
+
 // 同步创建
 function mkdirSync(pathUrl) {
   const pathArr = pathUrl.split("/");
@@ -45,6 +123,6 @@ function mkdir(pathUrl, callback) {
   next(0);
 }
 
-mkdir(pathUrl, function() {
-  console.log("mkdir success");
-});
+// mkdir(pathUrl, function() {
+//   console.log("mkdir success");
+// });
