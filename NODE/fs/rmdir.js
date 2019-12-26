987749cb003b08350192284022b3785032425bb9
NikoZhang <593177876@qq.com>
2019-12-26 18:32:25 +0800

    update

--- NODE/fs/rmdir.js
+++ NODE/fs/rmdir.js
@@ -2,10 +2,12 @@ const fs = require("fs");
 const path = require("path");
 
 // 同步删除文件夹，必须为空文件夹才能删除，递归至树叶节点，删除文件，或删除自身
-function rmdirSync(filePath) {
+function removeDirSync(filePath) {
   // 判断目标是否存在，
   const state = fs.statSync(filePath);
   const isDirectory = state.isDirectory();
+  console.log(state);
+
   if (isDirectory) {
     const childrenDirs = fs.readdirSync(filePath);
     // 删除子目录
@@ -14,7 +16,7 @@ function rmdirSync(filePath) {
         return path.resolve(filePath, item);
       })
       .forEach(item => {
-        rmdirSync(item);
+        removeDirSync(item);
       });
     // 删除自身
     fs.rmdirSync(filePath);
@@ -25,4 +27,4 @@ function rmdirSync(filePath) {
   return true;
 }
 
-rmdirSync(path.resolve(__dirname, "./c"));
+removeDirSync(path.resolve(__dirname, "./c"));
