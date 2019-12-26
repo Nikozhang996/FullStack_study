4ceb1ec0781fb4a1014dbccfa75776cd33d99cf0
Niko <593177876@qq.com>
2019-12-26 23:23:38 +0800

    update

--- NODE/fs/rmdir.js
+++ NODE/fs/rmdir.js
@@ -12,7 +12,7 @@ function removeDirSync(filePath) {
     const childrenDirs = fs.readdirSync(filePath);
     // 删除子目录
     childrenDirs
-      .map(function(item) {
+      .map(function (item) {
         return path.resolve(filePath, item);
       })
       .forEach(item => {
@@ -27,4 +27,36 @@ function removeDirSync(filePath) {
   return true;
 }
 
-removeDirSync(path.resolve(__dirname, "./c"));
+
+function removeDir(filePath, callback) {
+  fs.stat(filePath, function (err, targetStatus) {
+    if (targetStatus.isDirectory) {
+      fs.readdir(filePath, function (err, dirs) {
+        dirs
+          .map(function (item) {
+            return path.resolve(filePath, item);
+          })
+          .forEach(function (item) {
+            // 递归删除当前文件夹
+            console.log(item);
+
+          })
+      })
+    } else {
+      fs.unlink(filePath, typeof callback === 'function' && callback())
+    }
+  })
+}
+
+// removeDirSync(path.resolve(__dirname, "./c"));
+
+removeDir(path.resolve(__dirname, "./c"), function () {
+  // console.log(this);
+})
+
+/**
+ * https://gitee.com/jw-speed/201905jiagouke/blob/master/10.fs/2.fs-rmdir.js
+ * 1.检查是否存在
+ * 2.判断目标是否为文件夹还是文件
+ * 3.递归删除
+ */
