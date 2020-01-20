5d747ba2ddb6e5c5809b9de5cf651d97a22fef4d
NikoZhang <593177876@qq.com>
2020-01-20 14:41:32 +0800

    static-server

--- NODE/http/static-server.js
+++ NODE/http/static-server.js
@@ -1,41 +1,59 @@
 /**
  * https://gitee.com/speedly/201905jiagouke/blob/master/12.http/static-server.js
+ * resolve,join
  */
 const http = require("http");
 const url = require("url");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
+const { createReadStream } = require("fs");
 const mime = require("mime");
 
 class Server {
   constructor(config = {}) {
     this.port = config.port || 3000;
-    this.port = config.cwd || process.cwd();
+    this.cwd = config.cwd || process.cwd();
   }
   async handleRequest(req, res) {
-    // console.log(req);
-    // console.log(res);
+    const { pathname } = url.parse(req.url);
+    let filePath = path.join(this.cwd, pathname);
+
+    try {
+      const statObject = await fs.stat(filePath);
+      if (statObject.isDirectory()) {
+        filePath = path.join(filePath, "index.html");
+        await fs.access(filePath);
+      }
+      this.sendFile(req, res, filePath);
+    } catch (error) {
+      this.sendError(req, res, error);
+    }
   }
-  sendFile(req, res, filePath) {}
+  sendFile(req, res, filePath) {
+    res.setHeader("Content-Type", mime.getType(filePath) + ";charset=utf-8");
+    console.log(mime.getType(filePath));
+    createReadStream(filePath).pipe(res);
+  }
   sendError(req, res, err) {
     console.log(err);
     res.statusCode = 404;
     res.end("Not Found");
   }
   start() {
-    console.log(this);
-
     const server = http.createServer(this.handleRequest.bind(this));
     server.on("error", function(err) {
       console.log(err);
     });
-    server.listen(this.port, function() {
-      console.log(`服务器已运行于：${this.port}`);
-    });
+    server.listen(
+      this.port,
+      function() {
+        console.log(`服务器已运行于：${this.port}`);
+      }.bind(this)
+    );
   }
 }
 
-const server = new Server();
+const server = new Server({ port: 3000 });
 // const client = new Server({ port: 3000 });
 
 server.start();
