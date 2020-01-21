40fab36f3ac86922a5ef2382fa41774316ae3d4b
NikoZhang <593177876@qq.com>
2020-01-21 18:20:41 +0800

    gulp test

---
+++ gulp/gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp"),
+  sass = require("gulp-ruby-sass"),
+  cssnano = require("gulp-cssnano"),
+  autoprefixer = require("gulp-autoprefixer");
+(jshint = require("gulp-jshint")),
+  (uglify = require("gulp-uglify")),
+  (imagemin = require("gulp-imagemin")),
+  (rename = require("gulp-rename")),
+  (concat = require("gulp-concat")),
+  (cache = require("gulp-cache")),
+  (notify = require("gulp-notify")),
+  (livereload = require("gulp-livereload")),
+  (del = require("del"));
+
+console.log(autoprefixer);
