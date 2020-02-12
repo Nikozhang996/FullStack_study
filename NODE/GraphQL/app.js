ece801db22616ca732821860441643635de113c6
NikoZhang <593177876@qq.com>
2020-02-12 23:28:57 +0800

    [feature] init app

--- NODE/GraphQL/app.js
+++ NODE/GraphQL/app.js
@@ -7,12 +7,28 @@ const Koa = require('koa');
 const KoaStatic = require('koa-static');
 const KoaRouter = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const {ApolloServer} = require('apollo-server-koa');
+require('./mongodb');
+const routerMap = require('./router');
+const {typeDefs, resolvers} = require('./graphql/schema');
+const apollo = new ApolloServer({typeDefs, resolvers});
 
+// init
 const app = new Koa();
 const router = new KoaRouter();
-
 // 使用 bodyParser 和 KoaStatic 中间件
 app.use(bodyParser());
 app.use(KoaStatic(__dirname + '/public'));
+// 路由配置
+router.use(routerMap.routes());
+// 使用路由
+app.use(router.routes());
+app.use(router.allowedMethods());
+// 使用apollo
+app.use(apollo.getMiddleware());
 
+app.listen(4000, () => {
+  console.log('🚀 GraphQL-demo server listen at http://localhost:4000');
+  console.log(`🚀 Server ready at http://localhost:4000${apollo.graphqlPath}`);
+});
 // 路由设置test
