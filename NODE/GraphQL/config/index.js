d6880f742704699951971d315e15bbf98401b4c6
NikoZhang <593177876@qq.com>
2020-02-12 23:05:24 +0800

    [feature] init router

---
+++ NODE/GraphQL/config/index.js
@@ -0,0 +1,16 @@
+const router = require('koa-router')();
+
+// controllers
+const {saveInfo, fetchInfo} = require('../controllers/info');
+const {saveStudent, fetchStudent, fetchStudentDetail} = require('../controllers/student');
+const {saveCourse, fetchCourse} = require('../controllers/course');
+
+router.post('/saveinfo', saveInfo);
+router.get('/info', fetchInfo);
+router.post('/savestudent', saveStudent);
+router.get('/student', fetchStudent);
+router.get('/studentInfo', fetchStudentDetail);
+router.post('/savescourse', saveCourse);
+router.get('/course', fetchCourse);
+
+module.exports = router;
