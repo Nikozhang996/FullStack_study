4554a1fb869f70ed9810614693b25af63a1b62ac
Niko <593177876@qq.com>
2020-03-25 00:23:25 +0800

    update

---
+++ DesignPattern/装饰器.js
@@ -0,0 +1,36 @@
+Function.prototype.before = function (beforeFunc) {
+  const _this = this
+  return function () {
+    beforeFunc.apply(_this, arguments)
+    _this.apply(this, arguments)
+  }
+}
+
+Function.prototype.after = function (afterFunc) {
+  const _this = this
+  return function () {
+    _this.apply(this, arguments)
+    afterFunc.apply(_this, arguments)
+  }
+}
+
+const obj = {
+  fn() {
+    console.log(this);
+    
+    console.log('context');
+  }
+}
+
+function fn() {
+  console.log('context');
+}
+
+const func = obj.fn.before(function () {
+  console.log('before');
+}).after(function () {
+  console.log('after');
+})
+
+
+func()
