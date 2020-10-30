5ebaf35e7033e393771c785fa41d9b4f09117be2
Niko <593177876@qq.com>
2020-10-30 15:26:25 +0800

    update

--- TEST/工具函数.js
+++ TEST/工具函数.js
@@ -1,6 +1,36 @@
-const time = performance.timing;
+let source = {
+  a: 1,
+  b: undefined,
+  c: {
+    a: 2,
+    b: null,
+    d: {
+      a: undefined,
+      e: "",
+    },
+  },
+};
 
-function find(fn, ctx) {
-  fn = fn.bind(ctx)
-    
+let result = {};
+let temp = null;
+
+function handler(source) {
+  for (const key in source) {
+    const element = source[key];
+    if (
+      typeof element === "object" &&
+      element.constructor().toString() === "[object Object]"
+    ) {
+      console.log(element);
+    }
+    if (typeof element === "string" && element.length === 0) {
+    }
+    if (typeof element === "string" && element.length === 0) {
+    }
+    if (typeof element === "string" && element.length === 0) {
+    }
+  }
 }
+
+handler(source);
+console.log(result);
