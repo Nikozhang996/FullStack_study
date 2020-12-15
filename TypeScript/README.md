9d8445fd57a2cb09da03e21a1acfd5ba2f701e6b
NikoZhang <593177876@qq.com>
2020-12-15 21:08:10 +0800

    update

---
+++ TypeScript/README.md
@@ -0,0 +1,2 @@
+https://gitee.com/jw-speed/jiagouke3-vue/tree/master/12.ts-lesson/src
+
--- TypeScript/class.ts
+++ TypeScript/class.ts
@@ -3,10 +3,34 @@ ES6类，class，实例属性方法，原型属性方法，静态属性方法
 */
 
 class Pointer {
-  public x!: number;
+  public x: number;
   public y: number;
-  constructor(x: number, y?: number, ...args: number[]) {
+  constructor(x: number, y?: number, ...args: (number | string)[]) {
     this.x = x;
     this.y = y;
   }
+}
+
+const pointer = new Pointer(1, 2, 3, 4, 5, 6);
+
+class Animal {
+  static type = "动物";
+  static getType() {
+    return "动物类";
+  }
+
+  private name: string;
+  public age: number;
+  protected constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  say() {
+    console.log("say");
+  }
+}
+
+class Cat extends Animal {
+  constructor(parameters) {}
 }
