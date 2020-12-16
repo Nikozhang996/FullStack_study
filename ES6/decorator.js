0855e5065af05de2353f73bd72ac45d125d1909a
Niko <593177876@qq.com>
2020-12-16 18:11:22 +0800

    update

--- ES6/decorator.js
+++ ES6/decorator.js
@@ -1,18 +1,33 @@
-function CheckerDecorator(type) {
-  return function (target, name, descriptor) {
-    let v = descriptor.initializer && descriptor.initializer.call(this);
-    return {
-      enumerable: true,
-      configurable: true,
-      get: function () {
-        return v;
-      },
-      set: function (c) {
-        // 在此对传入的 c 的值做各种检查
-        var cType = typeof c;
-        // ...
-        v = c;
-      },
-    };
-  };
+/* 
+https://segmentfault.com/p/1210000009968000/read
+ */
+function decorateArmour(target, key, descriptor) {
+  console.log(target);
+  console.log(key);
+  console.log(descriptor);
 }
+
+class Man {
+  constructor(def = 2, atk = 3, hp = 3) {
+    this.init(def, atk, hp);
+  }
+
+  def = 0;
+  atk = 0;
+  hp = 0;
+
+  @decorateArmour
+  init(def, atk, hp) {
+    this.def = def;
+    this.atk = atk;
+    this.hp = hp;
+  }
+
+  getValue() {
+    return `防御力:${this.def},攻击力:${this.atk},血量:${this.hp}`;
+  }
+}
+
+const tom = new Man(8, 10, 18);
+
+console.log(tom.getValue());
