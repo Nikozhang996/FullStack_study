9943fde20fb3583bc634c8844c91bb4218e77583
NikoZhang <593177876@qq.com>
2021-02-15 22:44:32 +0800

    update

--- Performance/js/a.js
+++ Performance/js/a.js
@@ -1,9 +1,29 @@
 function logCount() {
   let count = 0;
 
-  for (let i = 0; i < 10 * 1000 * 1000 * 10; i++) {
+  for (let i = 0; i < 10 * 1000 * 1000 * 80; i++) {
     count += i;
   }
 
   console.log(count);
 }
+
+
+function a() {
+  b();
+}
+
+function b() {
+  c();
+}
+
+function c() {
+  d();
+}
+
+function d() {
+  logCount();
+}
+
+
+a();
